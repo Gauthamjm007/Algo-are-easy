4bf93f1d9e23dc0053504e8b9799847b5116aba0
Gauthamjm007 <j.mgautham15@gmail.com>
2020-02-18 22:26:23 +0530

    Updates

---
+++ precache-manifest.7796c5711b40d0fdcee04ef76997a104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2847e094e647dcd182933d1cd8d237",
+    "url": "/Algo-are-easy/index.html"
+  },
+  {
+    "revision": "dc8a5e2f62fb93170224",
+    "url": "/Algo-are-easy/static/css/main.6b3add77.chunk.css"
+  },
+  {
+    "revision": "8503016f4d36a1148b62",
+    "url": "/Algo-are-easy/static/js/2.e1ec0d28.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Algo-are-easy/static/js/2.e1ec0d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8a5e2f62fb93170224",
+    "url": "/Algo-are-easy/static/js/main.8f66e68b.chunk.js"
+  },
+  {
+    "revision": "597a04bea8e8524672c4",
+    "url": "/Algo-are-easy/static/js/runtime-main.7542be30.js"
+  }
+]);
